0a093c63495373177276124e7448e5ff6d703fd0
Sebastián Décima <sdecima@gmail.com>
2013-11-02 01:08:54 -0300

    Cambio de nombre a Activity para que lo tome el wizard al importar el proyecto

--- ejemplos/HolaMundo/src/com/example/holamundo/ActivityHolaMundo.java
+++ ejemplos/HolaMundo/src/com/example/holamundo/ActivityHolaMundo.java
@@ -4,7 +4,7 @@ import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
 
-public class MainActivity extends Activity {
+public class ActivityHolaMundo extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
